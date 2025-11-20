5fa050bf5b4ffd073c8b6237b8493a270b08c6bb
24e1810a-debug <24e1810a@shinshu-u.ac.jp>
2025-11-20 17:05:32 +0900

    Update joho.py

--- joho.py
+++ joho.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.title("こんばんは")
-option = st.selectbox('好きな数字を教えてください',list(['1番','2番','3番','4番']))
+option = st.selectbox('好きな数字を教えてください',list(['元気いっぱい','ちょっと疲れた','さっぱりしたい','こってりしたい',"落ち込んでい"]))
+input("あなたが選択したのは,") + option + 'です'
+
